8ad11949e48ed371eca2918cbccc19baaa2cf5d6
hoyatsui <2737077693@qq.com>
2021-01-21 15:50:30 +0800

    Ver_01: Completed ArrayDeque.java and LinkedListDeque.java

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,127 @@
+public class ArrayDeque<HakunaMatata> {
+    HakunaMatata[] items;
+    int size;
+    int nextFirst;
+    int nextLast;
+    int REFACTOR = 10;
+
+    /* create an empty list.*/
+    public ArrayDeque() {
+        items = (HakunaMatata[]) new Object[8];
+        nextFirst = 0;
+        nextLast = 0;
+        size = 0;
+    }
+
+    public ArrayDeque(ArrayDeque<HakunaMatata> other){
+        items = (HakunaMatata[]) new Object[8];
+        nextFirst = 3;
+        nextLast = 3;
+        size = 0;
+
+        for(int i = 0; i < other.items.length; i++){
+            addLast(other.get(i));
+        }
+    }
+
+    public void resize(int capacity) {
+        HakunaMatata[] a = (HakunaMatata[]) new Object[capacity];
+        if (nextLast > nextFirst) {
+            System.arraycopy(items, nextFirst, a, nextFirst, nextLast - nextFirst);
+        } else {
+            System.arraycopy(items, 0, a, 0, nextLast);
+            System.arraycopy(items, nextFirst, a, nextFirst + capacity - items.length, items.length - nextFirst);
+            nextFirst = nextLast + capacity - items.length;
+        }
+
+        items = a;
+    }
+
+    public void addFirst(HakunaMatata x) {
+        if (size == items.length) {
+            resize(size * REFACTOR);
+        }
+        if (nextFirst == 0) {
+            nextFirst = items.length - 1;
+        } else {
+            nextFirst -= 1;
+        }
+        items[nextFirst] = x;
+        size += 1;
+    }
+
+    public void addLast(HakunaMatata x) {
+        if (size == items.length) {
+            resize(size * REFACTOR);
+        }
+        if (nextLast == items.length - 1) {
+            items[nextLast] = x;
+            nextLast = 0;
+        } else {
+            items[nextLast] = x;
+            nextLast += 1;
+        }
+        size += 1;
+    }
+
+    public HakunaMatata get(int index) {
+        return items[index];
+    }
+
+    public  HakunaMatata removeFirst(){
+        HakunaMatata temp = items[nextFirst];
+        items[nextFirst] = null;
+        if (nextFirst == items.length -1) {
+            nextFirst = 0;
+        } else {
+            nextFirst += 1;
+        }
+        size -= 1;
+        return temp;
+    }
+
+    public HakunaMatata removeLast() {
+        if (nextLast == 0) {
+            nextLast = items.length - 1;
+            HakunaMatata temp = items[items.length - 1];
+            items[items.length - 1] = null;
+            nextLast = items.length - 1;
+            size -= 1;
+            return temp;
+        } else {
+            HakunaMatata temp = items[nextLast - 1];
+            items[nextLast - 1] = null;
+            nextLast = nextLast - 1;
+            size -= 1;
+            return  temp;
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void printDeque(){
+        if (size != 0) {
+            if (nextLast > nextFirst) {
+                for (int i = nextFirst; i < nextLast; i++) {
+                    System.out.println(items[i]);
+                }
+            } else {
+                for (int i = nextFirst; i < items.length; i++) {
+                    System.out.println(items[i]);
+                }
+                for (int i = 0; i < nextLast; i++) {
+                    System.out.println(items[i]);
+                }
+            }
+        } else {
+            System.out.println("List is empty!");
+        }
+    }
+
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,94 @@
+public class LinkedListDeque<T> {
+
+    public class StuffNode {
+        T items;
+        StuffNode next;
+        StuffNode prev;
+
+        public StuffNode(T item, StuffNode f, StuffNode n) {
+            items = item;
+            prev = f;
+            next = n;
+        }
+    }
+
+    private StuffNode sentinel;
+    private StuffNode last;
+    private int size;
+
+    public LinkedListDeque() {
+        sentinel = new StuffNode(null, null, null);
+        last = sentinel;
+        size = 0;
+    }
+
+    public LinkedListDeque(T item) {
+        sentinel = new StuffNode(null, null, null);
+        last = new StuffNode(item, null, null);
+        sentinel.next = last;
+        last.prev = sentinel;
+        size = 1;
+    }
+
+    public LinkedListDeque(LinkedListDeque<T> other) {
+        sentinel = new StuffNode(null, null, null);
+        last = sentinel;
+        size = 0;
+
+        for (int i = 0; i < other.size(); i++) {
+            addLast(other.get(i));
+        }
+    }
+
+    public void addFirst(T item) {
+        sentinel.next = new StuffNode(item, sentinel, sentinel.next);
+        last = sentinel.next;
+        size += 1;
+    }
+
+    public void addLast(T item) {
+        last.next = new StuffNode(item, last, null);
+        last = last.next;
+        size += 1;
+    }
+
+    public boolean isEmpty() {
+        return sentinel == last;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        StuffNode p = sentinel.next;
+        while (p != null) {
+            System.out.println(p.items);
+            p = p.next;
+        }
+    }
+
+    public T removeFirst() {
+        StuffNode temp = sentinel.next;
+        sentinel = sentinel.next;
+        size -= 1;
+        return temp.items;
+    }
+
+    public T removeLast() {
+        StuffNode temp = last;
+        last = last.prev;
+        size -= 1;
+        return temp.items;
+
+    }
+
+    public T get(int index) {
+        StuffNode temp = sentinel.next;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp.items;
+
+    }
+}
