e605e79e8d6bcfa9e6c1bb8322d3c300c117a9ee
hoyatsui <2737077693@qq.com>
2021-01-29 16:48:39 +0800

    Ver_02: Made the LLD a inner class of Palindrome due to the auto tester cannot complie LLD.java file

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -1,9 +1,114 @@
 
 public class Palindrome {
     /** A class for palindrome operations. */
+    static class LinkedListDeque<T> implements Deque<T> {
+
+        private class StuffNode {
+            T items;
+            StuffNode next;
+            StuffNode prev;
+
+            private StuffNode(T item, StuffNode p, StuffNode n) {
+                items = item;
+                prev = p;
+                next = n;
+            }
+        }
+
+        private StuffNode sentinel;
+        private StuffNode last;
+        private int size;
+
+        LinkedListDeque() {
+            sentinel = new StuffNode(null, null, null);
+            last = new StuffNode(null, null, null);
+            sentinel.next = last;
+            last.prev = sentinel;
+            size = 0;
+        }
+
+        @Override
+        public void addFirst(T item) {
+            sentinel.next.prev = new StuffNode(item, sentinel, sentinel.next);
+            sentinel.next = sentinel.next.prev;
+            size += 1;
+        }
+
+        @Override
+        public void addLast(T item) {
+            last.prev.next = new StuffNode(item, last.prev, last);
+            last.prev = last.prev.next;
+            size += 1;
+        }
+
+        @Override
+        public boolean isEmpty() {
+            return size == 0;
+        }
+
+        @Override
+        public int size() {
+            return size;
+        }
+
+        @Override
+        public void printDeque() {
+            StuffNode p = sentinel.next;
+            while (p.next != null) {
+                System.out.println(p.items);
+                p = p.next;
+            }
+        }
+
+        @Override
+        public T removeFirst() {
+            if (size == 0) {
+                return null;
+            }
+            StuffNode temp = sentinel.next;
+            sentinel.next = sentinel.next.next;
+            sentinel.next.prev = sentinel;
+            size -= 1;
+            return temp.items;
+        }
+
+        @Override
+        public T removeLast() {
+            if (size == 0) {
+                return null;
+            }
+            StuffNode temp = last.prev;
+            last.prev = last.prev.prev;
+            last.prev.next = last;
+            size -= 1;
+            return temp.items;
+
+        }
+
+        @Override
+        public T get(int index) {
+            StuffNode temp = sentinel.next;
+            for (int i = 0; i < index; i++) {
+                temp = temp.next;
+            }
+            return temp.items;
+        }
+
+        public T getRecursive(int index) {
+            return getRecursiveHelper(index, sentinel.next).items;
+        }
+
+        private StuffNode getRecursiveHelper(int index, StuffNode n) {
+            if (index == 0) {
+                return n;
+            }
+            return getRecursiveHelper(index - 1, n.next);
+        }
+    }
+
 
     public Deque<Character> wordToDeque(String word) {
-        LinkedListDeque<Character> deque = new LinkedListDeque<>();
+        Deque<Character> deque = new LinkedListDeque<>();
         for (int i = 0; i < word.length(); i++) {
             Character letter = word.charAt(i);
             deque.addLast(letter);
@@ -15,8 +120,8 @@ public class Palindrome {
         if (word.equals("")) {
             return true;
         }
-        LinkedListDeque<Character> deque = (LinkedListDeque<Character>) wordToDeque(word);
-        LinkedListDeque<Character> deque2 = new LinkedListDeque<>();
+        Deque<Character> deque = wordToDeque(word);
+        Deque<Character> deque2 = new LinkedListDeque<>();
         for (int i = 0; i < word.length(); i++) {
             Character letter = word.charAt(i);
             deque2.addFirst(letter);
@@ -30,7 +135,7 @@ public class Palindrome {
     }
 
     public boolean isPalindrome(String word, CharacterComparator cc) {
-        LinkedListDeque<Character> deque = (LinkedListDeque<Character>) wordToDeque(word);
+        Deque<Character> deque = (LinkedListDeque<Character>) wordToDeque(word);
         if ((word.length() % 2) != 0) {
             for (int i = 0; i < (word.length() - 1) % 2; i++) {
                 if (!cc.equalChars(deque.removeFirst(), deque.removeLast())) {
